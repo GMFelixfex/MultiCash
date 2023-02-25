77638b3365551cec913502fb15b3a727b930bb52
GMFelixfex <felix.poenitzsch@hs-furtwangen.de>
2023-02-25 03:24:38 +0100

    icon hd-ified

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -4,7 +4,7 @@ public static class ModHelperData
 {
     public const string WorksOnVersion = "35.0";
     public const string Version = "1.0.5";
-    public const string Name = "Multicash";
+    public const string Name = "MultiCash";
 
     public const string Description = "Allows you to Multiply the Cash gained from ANY Source<br><br>" +
         "F10: Set the Global Cash Multiplier<br><br>" +
@@ -13,5 +13,5 @@ public static class ModHelperData
         "Allows decimals like 1.5 or 0.5 if you want to make things harder<br>";
 
     public const string RepoOwner = "Felixfex"; 
-    public const string RepoName = "Multicash";
+    public const string RepoName = "MultiCash";
 }
